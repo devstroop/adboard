625e5addff3614de65d51645575dda658fc4a50e
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2024-04-18 00:08:48 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install -y --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
+sudo apt-get install -y --no-install-recommends chromium-browser
+
+
