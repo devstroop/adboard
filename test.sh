87b3cf16ecd921d4357eb589daa04a567304e47e
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2024-05-19 08:57:42 +0000

    new file:   test.sh

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# List of video URIs
+videos=("https://cdn.hallads.com/sample7.mp4" "https://cdn.hallads.com/sample8.mp4")
+
+# Function to play a video
+play_video() {
+    uri=$1
+    gst-launch-1.0 playbin uri=$uri
+}
+
+# Loop through the videos indefinitely
+while true; do
+    for video in "${videos[@]}"; do
+        play_video $video
+    done
+done
